bc58064071f73692d5afd7e5a67ddcfa41cdff51
Dan Misael <zepedamisael231@gmail.com>
2022-11-24 00:57:08 -0600

    Creación del Proyecto

---
+++ app/src/main/java/zepeda/misael/photogallery/PhotoRepository.kt
@@ -0,0 +1,18 @@
+package zepeda.misael.photogallery
+
+import retrofit2.Retrofit
+import retrofit2.converter.scalars.ScalarsConverterFactory
+import retrofit2.create
+import zepeda.misael.photogallery.api.FlickrApi
+
+class PhotoRepository {
+    private val flickrApi: FlickrApi
+    init {
+        val retrofit: Retrofit = Retrofit.Builder()
+            .baseUrl("https://www.flickr.com/")
+            .addConverterFactory(ScalarsConverterFactory.create())
+            .build()
+        flickrApi = retrofit.create()
+    }
+    suspend fun fetchContents() = flickrApi.fetchContents()
+}
---
+++ app/src/main/java/zepeda/misael/photogallery/api/FlickrApi.kt
@@ -0,0 +1,14 @@
+package zepeda.misael.photogallery.api
+
+import retrofit2.http.GET
+private const val API_KEY = "miapi"
+interface FlickrApi {
+    @GET(
+        "services/rest/?method=flickr.interestingness.getList" +
+                "&api_key=$API_KEY" +
+                "&format=json" +
+                "&nojsoncallback=1" +
+                "&extras=url_s"
+    )
+    suspend fun fetchPhotos(): String
+}
